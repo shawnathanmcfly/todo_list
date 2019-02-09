571b983f744153ab1f3521f57d45335615728240
Ethan Gent <ethan.gent.2391@gmail.com>
2019-02-09 11:42:14 -0500

    bug fix

---
+++ models/todo.js
@@ -0,0 +1,9 @@
+var sequelize = require("sequelize");
+
+module.exports = function(sequelize, DataTypes) {
+     var Todo = sequelize.define('Todo', {
+        text: DataTypes.STRING,
+        complete: DataTypes.BOOLEAN
+    });
+    return Todo;
+};
--- server.js
+++ server.js
@@ -13,7 +13,11 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-db.sequelize.sync()
+// db.sequelize.sync().then(function() {
+//   app.Listen(PORT, function() {
+//     console.log("listening on port: " + PORT)
+//   })
+// })
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
